48e7e7379850ecaf36356500976e5790b53bddbe
ajaysehwal <ajaysehwal000@gmail.com>
2024-12-30 16:35:22 +0530

    fix : minor docker errors

---
+++ chat21-web-widget/src/environments/environment.prod.ts
@@ -0,0 +1,39 @@
+// The file contents for the current environment will overwrite these during build2.
+// The build2 system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build2 --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: true,
+  version: require('../../package.json').version, // https://stackoverflow.com/questions/34907682/how-to-display-app-version-in-angular2
+  remoteConfig: true,
+  remoteConfigUrl: '/widget-config.json',
+  loadRemoteTranslations: true,
+  remoteTranslationsUrl: 'http://localhost:3000/',
+  chatEngine: 'CHANGEIT',
+  uploadEngine: 'CHANGEIT',
+  logLevel: 'INFO',
+  firebaseConfig: {
+    apiKey: 'CHANGEIT',
+    authDomain: 'CHANGEIT',
+    databaseURL: 'CHANGEIT',
+    projectId: 'CHANGEIT',
+    storageBucket: 'CHANGEIT',
+    messagingSenderId: 'CHANGEIT',
+    appId: 'CHANGEIT',
+    tenant: 'CHANGEIT',
+  },
+  chat21Config: {
+    appId: 'tilechat',
+    MQTTendpoint: 'mqtt://localhost:15675/ws', // MQTT endpoint
+    APIendpoint: 'http://localhost:8004/api'
+  },
+  apiUrl: 'http://localhost:3000/',
+  baseImageUrl: 'https://firebasestorage.googleapis.com/v0/b/',
+  dashboardUrl: 'https://console.tiledesk.com/v2/dashboard/',
+  defaultLang : 'en',
+  storage_prefix : 'widget_sv5',
+  authPersistence: 'LOCAL',
+  supportMode: true,
+  enbedJs: true
+};
---
+++ chat21-web-widget/src/environments/environment.ts
@@ -0,0 +1,40 @@
+// The file contents for the current environment will overwrite these during build2.
+// The build2 system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build2 --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  version: require('../../package.json').version, // https://stackoverflow.com/questions/34907682/how-to-display-app-version-in-angular2
+  remoteConfig: true,
+  remoteConfigUrl: '/widget-config.json',
+  loadRemoteTranslations: true,
+  remoteTranslationsUrl: 'http://localhost:3000/',
+  chatEngine: 'mqtt',
+  uploadEngine: 'native',
+  tenant: 'tilechat',
+  logLevel: 'INFO',
+  firebaseConfig: {
+    apiKey: 'CHANGEIT',
+    authDomain: 'CHANGEIT',
+    databaseURL: 'CHANGEIT',
+    projectId: 'CHANGEIT',
+    storageBucket: 'CHANGEIT',
+    messagingSenderId: 'CHANGEIT',
+    appId: 'CHANGEIT',
+    tenant: 'tilechat'
+  },
+  chat21Config: {
+    appId: 'tilechat',
+    MQTTendpoint: 'mqtt://localhost:15675/ws', // MQTT endpoint
+    APIendpoint: 'http://localhost:8004/api'
+  },
+  apiUrl: 'http://localhost:3000/',
+  baseImageUrl: 'https://firebasestorage.googleapis.com/v0/b/',
+  dashboardUrl: 'http://localhost:4500/',
+  defaultLang : 'en',
+  storage_prefix : 'widget_sv5',
+  authPersistence: 'LOCAL',
+  supportMode: true,
+  enbedJs: true
+};
